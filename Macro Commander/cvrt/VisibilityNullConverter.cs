124433379e1e190c7a2a74093d05ff4d423fb0c5
ShadowStep <Shadow-Step@mail.ru>
2018-08-21 17:27:18 +0300

    Convert null value to visibility

---
+++ Macro Commander/cvrt/VisibilityNullConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Macro_Commander
+{
+    class VisibilityNullConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return int.Parse(parameter as string) == 0 ? (value == null ? "Collapsed" : "Visible") : (value == null ? "Visible" : "Collapsed");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
