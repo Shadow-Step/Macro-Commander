9d6f7f1f798bf706620c19acef24d4b351628b6d
ShadowStep <Shadow-Step@mail.ru>
2018-08-22 19:02:33 +0300

    Param property and Validation check added

--- Macro Commander/src/Macro.cs
+++ Macro Commander/src/Macro.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Macro_Commander.src
 {
     [Serializable]
-    public class Macro : INotifyWrapper
+    public class Macro : INotifyWrapper , IDataErrorInfo
     {
         //Fields
         private string _name;
@@ -17,8 +19,10 @@ namespace Macro_Commander.src
         private bool _startedmutex = false;
         private double _totalExecutionTime;
         private bool _editingMode;
+        private string _params;
         //Properties
         public ObservableCollection<Action> Actions { get; set; }
+        public Dictionary<string, List<Action>> ActionsGroups { get; set; }
         public Action SelectedAction
         {
             get { return _selectedaction; }
@@ -72,12 +76,40 @@ namespace Macro_Commander.src
                 PropChanged("EditingMode");
             }
         }
+        public string Params
+        {
+            get { return _params; }
+            set
+            {
+                _params = value;
+                PropChanged();
+            }
+        }
         //Commands
         public RelayCommand CommandAddAction { get; set; }       
         public RelayCommand CommandDelAction { get; set; }
         public RelayCommand CommandExecute { get; set; }
         public RelayCommand CommandMoveForward { get; set; }
         public RelayCommand CommandMoveBackwards { get; set; }
+
+        //Validation check
+        public string Error => throw new NotImplementedException();
+        public string this[string columnName]
+        {
+            get
+            {
+                string error = string.Empty;
+                switch (columnName)
+                {
+                    case "Params":
+                        if (Params != null && Params != string.Empty && Regex.IsMatch(Params, ParamReader.PARAMS_PATTERN) == false)
+                            error = "Syntax error";
+                        break;
+                }
+                return error;
+            }
+        }
+
         //Constructor
         public Macro()
         {
@@ -87,6 +119,7 @@ namespace Macro_Commander.src
             CommandMoveForward = new RelayCommand(MoveActionForward);
             CommandMoveBackwards = new RelayCommand(MoveActionBackwards);
             Actions = new ObservableCollection<Action>();
+            ActionsGroups = new Dictionary<string, List<Action>>();
             Name = "New Macro";
         }
 
@@ -113,7 +146,29 @@ namespace Macro_Commander.src
             }
             
         }
-
+        private void ActionGroupChange(string group, Action action)
+        {
+            if(action.Group != string.Empty)
+            {
+                var list = ActionsGroups[action.Group];
+                list.Remove(action);
+                if (list.Count == 0)
+                {
+                    ActionsGroups[action.Group] = null;
+                    ActionsGroups.Remove(action.Group);
+                }
+                else
+                    list.Sort((x, y) => x.Index.CompareTo(y.Index));
+            }
+            if(group != string.Empty)
+            {
+                if (!ActionsGroups.ContainsKey(group))
+                    ActionsGroups.Add(group, new List<Action>());
+                var list = ActionsGroups[group];
+                list.Add(action);
+                list.Sort((x, y) => x.Index.CompareTo(y.Index));
+            }
+        }
         //Commands
         private void Execute(object param)
         {
@@ -128,18 +183,24 @@ namespace Macro_Commander.src
             {
                 Actions.Add(new Action(meta));
                 SelectedAction = Actions.Last();
+                SelectedAction.Index = Actions.Count - 1;
+                SelectedAction.GroupChangeEvent += ActionGroupChange;
             }
         }
         private void DelAction(object param)
         {
             if (param is Action action)
             {
+                action.GroupChangeEvent -= ActionGroupChange;
                 var index = Actions.IndexOf(action);
                 if (index < Actions.Count - 1)
                     SelectedAction = Actions[index + 1];
                 else if (index > 0)
                     SelectedAction = Actions[index - 1];
+                for (int i = index; i < Actions.Count; i++)
+                    Actions[i].Index--;
                 Actions.Remove(action);
+                
             }
         }
         private void StartMacro(object param)
@@ -154,6 +215,8 @@ namespace Macro_Commander.src
                 if (index < Actions.Count - 1)
                 {
                     Action temp = Actions[index + 1];
+                    temp.Index--;
+                    action.Index++;
                     Actions[index + 1] = Actions[index];
                     Actions[index] = temp;
                     SelectedAction = action;
@@ -167,7 +230,10 @@ namespace Macro_Commander.src
                 var index = Actions.IndexOf(action);
                 if (index > 0)
                 {
+                    
                     Action temp = Actions[index - 1];
+                    action.Index--;
+                    temp.Index++;
                     Actions[index - 1] = Actions[index];
                     Actions[index] = temp;
                     SelectedAction = action;
